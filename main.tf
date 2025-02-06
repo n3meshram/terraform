f3e9fc6fa1f4b0b4a97393429008bd39f2fb1f00
Nitin meshram <ntnmeshram1@gmail.com>
2025-02-06 20:37:11 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c50b6f7dc3701ddd" # Replace with your desired AMI ID
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "timepass"
+  }
+}
